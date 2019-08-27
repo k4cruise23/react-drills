518e5a7fd5c73ee231f3a98a3efcd7b54d11f4a5
Katie Cruise <katiecruise23@gmail.com>
2019-08-27 15:38:48 -0600

    completed app-2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,18 +3,18 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state ={
+      friends: ['Shay', 'Marcela', 'Corbin', 'Duncan', 'Lacey', 'Ash']
+    }
+  }
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    const displayFriends = this.state.friends.map((el, i) => {
+      return <h2 key={i}>{el}</h2>
+    })
+    return <div className='App'>{displayFriends}</div>
   }
 }
 
